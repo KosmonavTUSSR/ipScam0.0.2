f6d51b69086c496ce5379eafef1545cf94aef92f
KosmonavT <nico56ya@yandex.ru>
2022-03-16 16:53:48 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace ipScan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.BackColor = System.Drawing.Color.SteelBlue;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Gainsboro;
+            this.button1.Location = new System.Drawing.Point(537, 433);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(47)))), ((int)(((byte)(71)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox1.Font = new System.Drawing.Font("Cascadia Mono", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.ForeColor = System.Drawing.Color.AliceBlue;
+            this.textBox1.Location = new System.Drawing.Point(12, 28);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(584, 371);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(93)))), ((int)(((byte)(129)))));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button2.Enabled = false;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 11.25F);
+            this.button2.ForeColor = System.Drawing.Color.Gainsboro;
+            this.button2.Location = new System.Drawing.Point(653, 433);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 35);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "+Scam";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(47)))), ((int)(((byte)(71)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox2.Font = new System.Drawing.Font("Cascadia Mono", 11.25F);
+            this.textBox2.ForeColor = System.Drawing.Color.AliceBlue;
+            this.textBox2.Location = new System.Drawing.Point(593, 28);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(579, 371);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(13)))), ((int)(((byte)(0)))));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button3.Enabled = false;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 11.25F);
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(946, 433);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 35);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Firebrick;
+            this.label2.Location = new System.Drawing.Point(467, 602);
+            this.label2.MaximumSize = new System.Drawing.Size(251, 25);
+            this.label2.MinimumSize = new System.Drawing.Size(251, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(251, 25);
+            this.label2.TabIndex = 4;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button4
+            // 
+            this.button4.AutoSize = true;
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(93)))), ((int)(((byte)(129)))));
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 11.25F);
+            this.button4.ForeColor = System.Drawing.Color.Gainsboro;
+            this.button4.Location = new System.Drawing.Point(451, 433);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(80, 35);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "GO?";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Segoe UI", 11.25F);
+            this.checkBox1.ForeColor = System.Drawing.Color.Gainsboro;
+            this.checkBox1.Location = new System.Drawing.Point(169, 439);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(76, 24);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "RIPcash";
+            this.checkBox1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Segoe UI", 11.25F);
+            this.checkBox2.ForeColor = System.Drawing.Color.Gainsboro;
+            this.checkBox2.Location = new System.Drawing.Point(251, 439);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(94, 24);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "ClearBox1";
+            this.checkBox2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Font = new System.Drawing.Font("Segoe UI", 11.25F);
+            this.checkBox3.ForeColor = System.Drawing.Color.Gainsboro;
+            this.checkBox3.Location = new System.Drawing.Point(351, 439);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(94, 24);
+            this.checkBox3.TabIndex = 7;
+            this.checkBox3.Text = "ClearBox2";
+            this.checkBox3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.checkBox3.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.AutoSize = true;
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(204)))), ((int)(((byte)(31)))));
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Segoe UI", 11.25F);
+            this.button5.ForeColor = System.Drawing.Color.Transparent;
+            this.button5.Location = new System.Drawing.Point(1062, 433);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(110, 35);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "SaveYouSalfe";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(38)))), ((int)(((byte)(56)))));
+            this.ClientSize = new System.Drawing.Size(1184, 672);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(1200, 860);
+            this.MinimumSize = new System.Drawing.Size(1200, 680);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ip Scammm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ipScan
+{
+
+    public partial class Form1 : Form
+    {
+        readonly string savePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        private string ipListSec;
+        public string ipList;
+        static public string ipAdr;
+        static public string macAdr;
+        public Form1()
+         
+        {
+            InitializeComponent();
+        }
+        public void Start()
+        {
+            Process scan = new Process();
+            scan.StartInfo.FileName = "arp";
+            scan.StartInfo.Arguments = "-a";
+            scan.StartInfo.RedirectStandardOutput = true;
+            scan.StartInfo.UseShellExecute = false;
+            scan.StartInfo.CreateNoWindow = true;
+            scan.StartInfo.StandardOutputEncoding = Encoding.GetEncoding(866);
+            scan.Start();
+            ipList = scan.StandardOutput.ReadToEnd();
+            textBox1.Text = ipList;
+            button2.Enabled = true; //Scam+
+            button3.Enabled = true; //Clear
+        }
+        public void ClearCash()
+        {
+            if (checkBox1.Checked)
+            {
+                Process clear = new Process();
+                clear.StartInfo.FileName = "netsh";
+                clear.StartInfo.Arguments = "interface ip delete arpcache";
+                clear.StartInfo.UseShellExecute = false;
+                clear.StartInfo.CreateNoWindow = true;
+                clear.Start();
+            }
+            if (checkBox2.Checked)
+            {
+                textBox1.Text = "";
+            }
+            if (checkBox3.Checked)
+            {
+                textBox2.Text = "";
+            }
+            Comparison();
+        }
+        public void StartSec()
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = "arp";
+            process.StartInfo.Arguments = "-a";
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.StandardOutputEncoding = Encoding.GetEncoding(866);
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            process.Start();
+            ipListSec = process.StandardOutput.ReadToEnd();
+            textBox2.Text = ipListSec;
+        }
+        public void Clear()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            button2.Enabled = false;
+            label2.Text = "Данные удалены";
+        }
+        public void SaveFile()
+        {
+            if (textBox1.Text != "")
+            {
+                DialogResult result = MessageBox.Show("Ваш результат будет сохранен в файл", "Сохранение",
+                    MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+                if (result == DialogResult.OK)
+                {
+                    File.Exists($@"{savePath}\ipList.txt");
+                    if (File.Exists($@"{savePath}\ipList.txt"))
+                    {
+                        DialogResult result2 = MessageBox.Show("ipList.txt уже существует \n Заменить?", "Сохранение",
+                             MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                        if (result2 == DialogResult.Yes)
+                        {
+                            File.WriteAllText($@"{savePath}\ipList.txt", textBox1.Text + textBox2.Text);
+                        }
+                        else { }
+                    }
+                    else
+                    {
+                        File.WriteAllText($@"{savePath}\ipList.txt", textBox1.Text + textBox2.Text);
+                    }
+                }
+            }
+        }
+        public void Comparison()
+        {
+            if (textBox1.Text == textBox2.Text)
+            {
+                label2.ForeColor = Color.LimeGreen;
+                label2.Text = "Изменений не обнаружено";
+            }
+            else if (textBox2.Text == "")
+            {
+                label2.Text = "";
+            }
+            else
+            {
+                label2.ForeColor = Color.Firebrick;
+                label2.Text = "Данные не совпадают";
+            }
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Start();
+            Comparison();
+        }
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            StartSec();
+            Comparison();
+        }
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            Clear();
+            
+        }
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            ClearCash();
+        }
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            SaveFile();
+        }
+        // Cкролбары
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            Size sz = TextRenderer.MeasureText(textBox1.Text, Font);
+            textBox1.ScrollBars = sz.Height > textBox1.Height ? ScrollBars.Vertical : ScrollBars.None;
+        }
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            Size sz = TextRenderer.MeasureText(textBox2.Text, Font);
+            textBox2.ScrollBars = sz.Height > textBox2.Height ? ScrollBars.Vertical : ScrollBars.None;
+        }
+    }
+}
